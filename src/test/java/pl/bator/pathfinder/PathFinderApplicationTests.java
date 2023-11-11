43864237a66b5395fef4fa665533bb79d4167fd8
marcinbator <marcinbator.ofc@gmail.com>
2023-11-11 16:32:26 +0100

    basic maven and tests

--- src/test/java/pl/bator/pathfinder/PathFinderApplicationTests.java
+++ src/test/java/pl/bator/pathfinder/PathFinderApplicationTests.java
@@ -2,8 +2,10 @@ package pl.bator.pathfinder;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class PathFinderApplicationTests {
 
     @Test
