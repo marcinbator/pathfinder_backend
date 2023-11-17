70d373457ce609dddad27168608f2a858c3a9505
marcinbator <marcinbator.ofc@gmail.com>
2023-11-17 11:00:46 +0100

    microservices basic setup

---
+++ pathfinder_service/src/test/java/pl/bator/pathfinder_service/PathfinderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bator.pathfinder_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathfinderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
