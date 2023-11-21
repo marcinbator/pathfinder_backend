6113e1bea845f507765d07785a42ddd022e8e79d
marcinbator <marcinbator.ofc@gmail.com>
2023-11-21 18:44:59 +0100

    basic auth security setup

--- pathfinder_auth/src/main/java/pl/bator/pathfinder_service/ServiceController.java
+++ pathfinder_auth/src/main/java/pl/bator/pathfinder_service/ServiceController.java
@@ -2,6 +2,9 @@ package pl.bator.pathfinder_service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +17,19 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class ServiceController {
     private WebClient webClient;
 
-    @GetMapping("/{name}")
-    public ResponseEntity<String> isInDb(@PathVariable String name) {
+    @GetMapping("")
+    public ResponseEntity<String> isInDb() {
         return ResponseEntity.ok(webClient.get()
                 .uri("http://localhost:8080/api/record")
                 .retrieve()
                 .bodyToMono(String.class)
                 .block());
+    }
+
+    @GetMapping("/me")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<String> me() {
+        var user = (OidcUser)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ResponseEntity.ok(user.getEmail());
     }
 }
