e3cf5d3541944e703334a8c76993f7a4e9814a6a
marcinbator <marcinbator.ofc@gmail.com>
2023-11-21 19:20:42 +0100

    basic jwt workflow for client service

--- pathfinder_backend/src/main/java/pl/bator/pathfinder/infrastructure/record/RecordController.java
+++ pathfinder_backend/src/main/java/pl/bator/pathfinder/infrastructure/record/RecordController.java
@@ -2,6 +2,7 @@ package pl.bator.pathfinder.infrastructure.record;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class RecordController {
     private final RecordService recordService;
 
     @GetMapping
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<Record>> getRecords() {
         return ResponseEntity.ok(recordService.getRecords());
     }
